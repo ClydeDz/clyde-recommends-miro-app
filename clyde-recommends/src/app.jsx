cae210f1b7e6822b6d512f038ad5be3798421de2
Clyde D'Souza <clyd94@gmail.com>
2024-06-23 00:24:11 +1200

    Implement basic chat ui

--- clyde-recommends/src/app.jsx
+++ clyde-recommends/src/app.jsx
@@ -1,7 +1,309 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import "../src/assets/style.css";
+import {
+  MainContainer,
+  ChatContainer,
+  TypingIndicator,
+  MessageSeparator,
+  MessageList,
+  Avatar,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+
+const messages = [
+  {
+    type: "separator",
+    props: {
+      content: "Saturday, 30 November 2019",
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "single",
+      },
+      children: <Avatar src="/src/assets/congratulations.png" name="Eliot" />,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "single",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "first",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "normal",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "normal",
+      },
+      avatarSpacer: false,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "last",
+      },
+      children: <Avatar src={"/src/assets/congratulations.png"} name="Eliot" />,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "first",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "normal",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "normal",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "outgoing",
+        position: "last",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "first",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "last",
+      },
+      children: <Avatar src={"/src/assets/congratulations.png"} name="Eliot" />,
+    },
+  },
+  {
+    type: "separator",
+    props: {
+      content: "Saturday, 31 November 2019",
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "single",
+      },
+      children: <Avatar src={"/src/assets/congratulations.png"} name="Eliot" />,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "single",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "first",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "normal",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "normal",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "last",
+      },
+      children: <Avatar src={"/src/assets/congratulations.png"} name="Eliot" />,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "first",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "normal",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Zoe",
+        direction: "outgoing",
+        position: "normal",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "outgoing",
+        position: "last",
+      },
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+        position: "first",
+      },
+      avatarSpacer: true,
+    },
+  },
+  {
+    props: {
+      model: {
+        message: "Hello my friend",
+        sentTime: "15 mins ago",
+        sender: "Eliot",
+        direction: "incoming",
+      },
+      children: <Avatar src={"/src/assets/congratulations.png"} name="Eliot" />,
+    },
+  },
+];
 
 async function addSticky() {
   const stickyNote = await miro.board.createStickyNote({
@@ -12,33 +314,67 @@ async function addSticky() {
 }
 
 const App = () => {
-  // React.useEffect(() => {
-  //   addSticky();
-  // }, []);
+  const [userInfo, setUserInfo] = React.useState();
+  async function getInfo() {
+    const userInfo = await miro.board.getUserInfo();
+    setUserInfo(userInfo.name);
+  }
+
+  React.useEffect(() => {
+    getInfo();
+  }, []);
 
   return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
+    <>
+      <div className="grid wrapper">
+        {/* <div className="cs1 ce12">
+          <p>ehfuwheifh</p>
+        </div> */}
+        <div className="cs1 ce12">
+          <MainContainer>
+            <ChatContainer>
+              <MessageList
+                style={{ height: "500px" }}
+                typingIndicator={
+                  <TypingIndicator content={`${userInfo} is typing`} />
+                }
+              >
+                {messages.map((m, i) =>
+                  m.type === "separator" ? (
+                    <MessageSeparator key={i} {...m.props} />
+                  ) : (
+                    <Message key={i} {...m.props}>
+                      {" "}
+                      <Message.Footer
+                        content="Youu"
+                        style={{ display: "flex", justifyContent: "flex-end" }}
+                      >
+                        You
+                      </Message.Footer>
+                    </Message>
+                  )
+                )}
+              </MessageList>
+              <MessageInput placeholder="Type message here" />
+            </ChatContainer>
+          </MainContainer>
+          {/* <MainContainer>
+            <ChatContainer>
+              <MessageList>
+                <Message
+                  model={{
+                    message: "Hello my friend",
+                    sentTime: "just now",
+                    sender: "Joe",
+                  }}
+                />
+              </MessageList>
+              <MessageInput placeholder="Type message here" />
+            </ChatContainer>
+          </MainContainer> */}
+        </div>
       </div>
-      <div className="cs1 ce12">
-        <h1>!@!@!@!@!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
-      </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
-        >
-          Read the documentation
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ miro.js
@@ -0,0 +1,63 @@
+// https://www.radix-ui.com/themes/docs/components/aspect-ratio
+// https://azure.github.io/communication-ui-library/?path=/story/ui-components-messagethread--message-thread&args=richTextEditor:true
+// https://ui.shadcn.com/docs/figma
+// https://github.com/chatscope/chat-ui-kit-react?tab=readme-ov-file
+// Clyde's Template Recommender
+// Clyde Recommends
+
+const miro = [
+  {
+    title: "Gratitude Journal",
+    url: "https://miro.pxf.io/1r4gxD",
+    image: "",
+    tags: [
+      "Gratitude Journal",
+      "Miro template",
+      "daily gratitude",
+      "mindfulness journal",
+      "gratitude exercise",
+      "personal reflection",
+      "daily journaling",
+      "mental health template",
+      "gratitude practice",
+      "well-being journal",
+      "reflection journal",
+      "journaling template",
+      "digital journal",
+      "gratitude doodles",
+      "daily reflection",
+      "Miro journaling",
+      "mental health practice",
+      "emotional well-being",
+      "daily mindfulness",
+      "printable journal template",
+    ],
+  },
+  {
+    title: "A Hive-ly Retrospective!",
+    url: "https://miro.pxf.io/B04GqL",
+    image: "",
+    tags: [
+      "Hively Retrospective",
+      "Miro template",
+      "retrospective board",
+      "bee-themed retrospective",
+      "team reflection",
+      "agile retrospective",
+      "team success",
+      "lesson sharing",
+      "post-it notes",
+      "buzzkill moments",
+      "team collaboration",
+      "innovation reflection",
+      "World Bee Day",
+      "team achievements",
+      "future planning",
+      "agile workflows",
+      "team workshops",
+      "digital retrospective",
+      "reflection exercise",
+      "Miro retrospective",
+    ],
+  },
+];
