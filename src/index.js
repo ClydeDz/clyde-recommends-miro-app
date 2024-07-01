0f4f3c23aa99beb3ae1054ec00a594f418429464
Clyde D'Souza <clyd94@gmail.com>
2024-07-02 00:32:28 +1200

    Send event when chat event is closed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import mixpanel from "mixpanel-browser";
+
 export async function init() {
-  miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
+  miro.board.ui.on("icon:click", async () => {
+    const { waitForClose } = await miro.board.ui.openPanel({ url: "app.html" });
+
+    await waitForClose().then(() => { 
+      mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN, {
+        debug: false,
+        track_pageview: false,
+        persistence: "localStorage",
+      });
+      mixpanel.track("Chat ended", {});
+    });
   });
 }
 
